3732af04022cb27a3b39546685c20ab62d671cbf
Lucas A <98680825+lucasDOarruda@users.noreply.github.com>
2023-03-19 14:08:56 +1100

    changes commit build a interface
    a

--- script.js
+++ script.js
@@ -1,14 +1,66 @@
-const sections = document.querySelectorAll('.fade-in');
-const sectionObserver = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('active');
-        }
-    });
-});
-sections.forEach(section => {
-    sectionObserver.observe(section);
+const nightModeSwitch = document.querySelector('#night-mode-switch');
+const body = document.querySelector('body');
+
+nightModeSwitch.addEventListener('click', function() {
+  nightModeSwitch.classList.toggle('clicked');
+  body.classList.toggle('night-mode');
 });
 
 
 
+function openPopup() {
+  document.getElementById("popup").style.display = "block";
+}
+
+function closePopup() {
+  document.getElementById("popup").style.display = "none";
+}
+
+
+function toggleDetails() {
+  var details = document.querySelector("#skills-details");
+  var toggleButton = document.querySelector(".toggle-details");
+  if (details.style.display === "none") {
+    details.style.display = "block";
+    toggleButton.textContent = "-";
+  } else {
+    details.style.display = "none";
+    toggleButton.textContent = "+";
+  }
+}
+
+
+
+
+
+
+const scrollToTopBtn = document.querySelector('#scroll-to-top');
+
+scrollToTopBtn.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: 'smooth'
+  });
+});
+
+
+
+
+
+
+// Get the flowthing element and the close button
+var flowthing = document.getElementById('flowthing');
+var closeBtn = document.querySelector('.close-btn');
+
+// Add click event listener to the flowthing to show the popup
+flowthing.addEventListener('click', function(event) {
+  event.preventDefault();
+  document.getElementById('myPopup').style.display = 'block';
+});
+
+// Add click event listener to the close button to hide the popup
+closeBtn.addEventListener('click', function(event) {
+  event.preventDefault();
+  document.getElementById('myPopup').style.display = 'none';
+});
